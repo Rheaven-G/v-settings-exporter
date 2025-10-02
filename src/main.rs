5b629ced5c179e467fb709f0060c8c36d86c3421
V LABROUSSE <vallab.master@proton.me>
2025-10-01 23:40:58 -0400

    Made a bare bone event loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+// Main file of vSettingsExporter
+const PNAME:&str = "vSetEx"; // Program name printed in terminal
+
 fn main() {
-    println!("Hello, world!");
+    // We do everything we need to before starting the program
+    println!("{PNAME}] Starting !");
+
+    // We then start the program and its event loop
+    main_event_loop();
+
+    safely_closing();
+}
+
+fn main_event_loop() {
+    let mut running:bool = true;
+
+    let mut count:u32 = 5;
+
+    // Main even loop
+    while running {
+        println!("{PNAME}] {count}...");
+
+
+        if count > 0 {
+            count = count - 1;
+        }
+        else {
+            running = false;
+        }
+    }
+}
+
+fn safely_closing() {
+    println!("{PNAME}] Closed safely !");
 }
