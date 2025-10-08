0f91d02ac1c993a4665346bb1305b24580f1b512
V Cassel <dev.vcass@pm.me>
2025-10-08 12:37:32 -0400

    CLI Log and Colour system

---
+++ python/src/cmdinterpreter.py
@@ -0,0 +1,4 @@
+# Used for command line interpreter
+
+def cmd_loop():
+    print("vSetEx CLI now running.")
--- python/src/main.py
+++ python/src/main.py
@@ -1,1 +1,25 @@
-# 
+# Main Python for vSettingsExporter
+
+import cmdinterpreter
+from utils import log, debug_log_colours, MsgType
+
+
+env = { # programme environmental variables
+    "CFG": '~/.vsetex/config',
+    "SET_DICT": '~/.vsetex/settings'
+}
+
+def main():
+    log("Starting vSettings Exporter...", "", MsgType.TITLE)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+    # cmdinterpreter.cmd_loop()
+    debug_log_colours()
